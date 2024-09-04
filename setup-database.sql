a1f45ee2bcd702e516194175cf6a56d7c77386c7
Kiwirish <blakeleahy7@gmail.com>
2024-09-05 00:05:01 +1200

    Database issue fix

--- setup-database.sql
+++ setup-database.sql
@@ -5,5 +5,14 @@ CREATE TABLE items (
   date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE laundry_bookings (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  booked_by VARCHAR(50) NOT NULL,
+  booking_date DATE NOT NULL
+);
+
+
 INSERT INTO items (item_name, added_by) VALUES ('Milk', 'User1');
 INSERT INTO items (item_name, added_by) VALUES ('Bread', 'User2');
+
+INSERT INTO laundry_bookings (booked_by, booking_date) VALUES ('Blake', '20-10-2024');
